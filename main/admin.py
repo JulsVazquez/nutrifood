1b81694718d795507bf2e32ff9e47d5bc90e987f
Victor Gallegos <316160456@ciencias.unam.mx>
2022-04-23 15:17:41 -0500

    Crear recetas y models para test

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, Recipe
+
+
+admin.site.register(Recipe)
+admin.site.register(Topic)
--- nutrifood/settings.py
+++ nutrifood/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+
+    'main',
 ]
 
 SITE_ID = 1
