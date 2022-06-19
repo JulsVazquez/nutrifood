9309beb81995faad6eaeaa2686a6166992c37227
VChristianPimentel <VChristianPimentel>
2022-06-18 23:50:26 -0500

    Daily diet done and detail, front end lacks details

--- nutrifood/urls.py
+++ nutrifood/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('esp/', include('displayapi.urls')),
     path('users/', include('users.urls')),
+    path('diets/', include('diets.urls')),
 ]
