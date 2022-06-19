5b2d3f336a65ec27be11b77a8f17799f6b2837f9
Victor Gallegos <316160456@ciencias.unam.mx>
2022-06-19 14:39:14 -0500

    Reparando vista home, search, detalles y api

--- diets/views.py
+++ diets/views.py
@@ -15,7 +15,7 @@ def diet(request):
     tags = ["breakfast", "brunch", "lunch", "dinner"]
     response = {'recipes': []}
     for tag in tags:
-        url = f'https://api.spoonacular.com/recipes/random?apiKey=308c4ea0ef67478eace9c54c39a12c43&number=1&tag={tag},healthy'
+        url = f'https://api.spoonacular.com/recipes/random?apiKey=717a16dc5e4f4d50b4a5ad4eaf0ca926&number=1&tag={tag},healthy'
         temp = session.get(url=url).json()
         temp['recipes'][0]['tag'] = tag
         response['recipes'].extend(temp['recipes'])
@@ -28,7 +28,7 @@ def detail(request, id):
     session = requests.Session()
     session.headers.update({'Content-Type': 'application/json',
                            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'})
-    url = f'https://api.spoonacular.com/recipes/{id}/information?apiKey=308c4ea0ef67478eace9c54c39a12c43'
+    url = f'https://api.spoonacular.com/recipes/{id}/information?apiKey=717a16dc5e4f4d50b4a5ad4eaf0ca926'
     response = session.get(url=url).json()
     response['protein'] = re.findall(
         r'\d{1,}g of protein', response['summary'])[0].split()[0]
