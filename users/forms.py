f41583469ce7000bbae3d8b61f0e3171a4022c81
Victor Gallegos <316160456@ciencias.unam.mx>
2022-06-19 21:14:10 -0500

    Mejorando diseño de vistas

--- users/forms.py
+++ users/forms.py
@@ -3,11 +3,12 @@ from allauth.account.forms import SignupForm
 from django import forms
 from pkg_resources import require
 
+
 class CustomSignupForm(SignupForm):
-    weight = forms.IntegerField(label='Peso actual (kg)', min_value = 0)
-    height = forms.IntegerField(label = 'Altura (cm)', min_value = 0)
-    is_vegan = forms.BooleanField(label = 'Vegan', required = False)
-    is_keto = forms.BooleanField(label = 'Keto', required = False)
+    weight = forms.IntegerField(label='Weight (kg)', min_value=0)
+    height = forms.IntegerField(label='Height (cm)', min_value=0)
+    is_vegan = forms.BooleanField(label='Vegan', required=False)
+    is_keto = forms.BooleanField(label='Keto', required=False)
 
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
